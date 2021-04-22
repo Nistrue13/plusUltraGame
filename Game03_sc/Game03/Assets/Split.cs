5835f506f2ba05fc2db65691d3cab243b6858e07
Victor Franco <vfranco.itc@gmail.com>
2021-04-21 19:26:44 -0500

    Added split screen
    Co-Authored-By: ElMike1712 <54449523+elmike1712@users.noreply.github.com>
    Co-Authored-By: Juan Pablo Ortiz <80001672+BeanIRL@users.noreply.github.com>
    Co-Authored-By: Horacio <57412919+A01367213@users.noreply.github.com>

---
+++ Game03_sc/Game03/Assets/Split.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Split : MonoBehaviour{
+
+    public Camera cam1, cam2;
+
+    private bool isHorizontalSplit;
+
+    void Start(){
+        
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.E)){
+            SwitchView();
+        }
+    }
+
+    public void SwitchView(){
+        isHorizontalSplit = !isHorizontalSplit;
+
+        SetSplitScreen();
+    }
+
+    public void SetSplitScreen(){
+        if(isHorizontalSplit){
+
+            cam1.rect = new Rect(0f, .5f, 1f, .5f);
+            cam2.rect = new Rect(0f, 0f, 1f, .5f);
+
+        } else{
+
+            cam1.rect = new Rect(0f, 0f, .5f, 1f);
+            cam2.rect = new Rect(.5f, 0f, .5f, 1f);
+
+        }
+    }
+}
+
